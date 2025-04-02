665f72b105c9276d0e70ade076ae1432c296bf74
Anastasia Chuvilova <achuvilova098@gmail.com>
2025-04-02 09:51:29 +0500

    Упрощен конструктор класса Movie

--- src/movie.ts
+++ src/movie.ts
@@ -1,34 +1,14 @@
 export default class Movie {
-    id: number;
-    name: string; 
-    nameEng: string; 
-    year: number;
-    country: string; 
-    slogan: string; 
-    genre: string;
-    time: string;
-    price: number;
     
     constructor(
-        id: number, 
-        name: string, 
-        nameEng: string, 
-        year: number, 
-        country: string, 
-        slogan: string, 
-        genre: string, 
-        time: string,
-        price: number,
-    )   {
-            this.id = id;
-            this.name = name;
-            this.nameEng = nameEng;
-            this.year = year;
-            this.country = country;
-            this.slogan = slogan;
-            this.genre = genre;
-            this.genre = genre;
-            this.time = time;
-            this.price = price;
-        }
+       readonly id: number, 
+       readonly name: string, 
+       readonly nameEng: string, 
+       readonly year: number, 
+       readonly country: string, 
+       readonly slogan: string, 
+       readonly genre: string, 
+       readonly time: string,
+       readonly price: number,
+    ){}
 }
